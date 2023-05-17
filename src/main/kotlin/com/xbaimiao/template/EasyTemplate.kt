9bbedea42a4d564b63f10c6316357314de5dfc12
小白 <67080574+xbaimiao@users.noreply.github.com>
2023-05-17 09:43:04 +0800

    Initial commit

---
+++ src/main/kotlin/com/xbaimiao/template/EasyTemplate.kt
@@ -0,0 +1,24 @@
+package com.xbaimiao.template
+
+import com.xbaimiao.easylib.EasyPlugin
+
+@Suppress("unused")
+class EasyTemplate : EasyPlugin() {
+
+    override fun load() {
+        
+    }
+
+    override fun enable() {
+
+    }
+
+    override fun active() {
+
+    }
+
+    override fun disable() {
+
+    }
+
+}
